c728a18045d07648d988612a71658e284f335947
buzzcola3 <buzzcola3@gmail.com>
2021-05-18 10:44:15 +0000

    modified:   AndroidProducts.mk 	renamed:    lineage_santoni.mk -> potato_santoni.mk 	deleted:    touch/Android.bp 	deleted:    touch/KeyDisabler.cpp 	deleted:    touch/KeyDisabler.h 	deleted:    touch/service.cpp 	deleted:    touch/vendor.lineage.touch@1.0-service.xiaomi_8937.rc 	deleted:    touch/vendor.lineage.touch@1.0-service.xiaomi_8937.xml

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/potato_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    potato_santoni-userdebug \
+    potato_santoni-user \
+    potato_santoni-eng
